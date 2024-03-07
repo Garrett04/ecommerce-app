3fb0039c10f80af4305100f36c2b1d9190e9f2c9
garrett04 <garrettcorreia21@gmail.com>
2024-03-07 16:58:45 +0000

    Setup Register Page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './resources/css/reset.css';
 import './index.css';
+import './pages/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,42 @@
+import { useCallback, useState } from "react"
+import { Link } from "react-router-dom";
+
+
 const Register = () => {
-  return (
-    <h1>Register Page</h1>
-  )
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleChange = useCallback((e) => e.target.name === 'username' ? setUsername(e.target.value) : setPassword(e.target.value), []);
+
+    return (
+      <div className="register">
+        <h1>Register Account</h1>
+        <form action="/api/register" method="POST">
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text" 
+              name="username" 
+              id="username" 
+              value={username} 
+              onChange={handleChange} 
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>{" "}
+            <input 
+              type="password" 
+              name="password"
+              id="password" 
+              value={password} 
+              onChange={handleChange} 
+            />
+          </div>
+          <input type="submit" value="Register" />
+        </form>
+        <p>Already Registered? <Link to="/login">Login here</Link></p>
+      </div>
+    )
 }
 export default Register
