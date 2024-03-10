e38190d47a0d3a3247826a70caa1288ac8acbbe3
garrett04 <garrettcorreia21@gmail.com>
2024-03-10 13:15:50 +0000

    Add header logo link to homepage

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,11 +2,12 @@ import Cart from "./CartButton"
 import SearchBar from "./SearchBar"
 import SignInButton from "./AccountButton"
 import Logout from "./LogoutButton"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <div className="header">
-      <h1><span>S</span>hop<span>W</span>ise</h1>
+      <h1><a href="/"><span>S</span>hop<span>W</span>ise</a></h1>
       <SearchBar />
       <div className="right">
         <Cart />
