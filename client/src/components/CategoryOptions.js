abebf48957e828b9d1a751e541d3b79a928bcfc8
garrett04 <garrettcorreia21@gmail.com>
2024-03-10 22:17:18 +0000

    Add category options using the api

--- client/src/components/CategoryOptions.js
+++ client/src/components/CategoryOptions.js
@@ -1,9 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getCategoriesError, getCategoriesStatus, selectCategories } from "../features/categories/categoriesSlice";
+import { useEffect } from "react";
+import { fetchCategories } from "../apis/categories";
 
 
 const CategoryOptions = () => {
-  return (
-    <p>CategoryOptions component here</p>    
-  );
+    const categories = useSelector(selectCategories);
+    const categoriesStatus = useSelector(getCategoriesStatus);
+    const categoriesError = useSelector(getCategoriesError);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      if (categoriesStatus === 'idle') {
+        dispatch(fetchCategories());
+      }
+    }, [dispatch]);
+
+    const renderCategories = () => {
+      return categories.map(category => (
+        <li key={category.id} className="category">
+          {category.name}
+        </li>
+      ))
+    }
+
+    let content;
+    if (categoriesStatus === 'pending') {
+      content = 'Loading...';
+    } else if (categoriesStatus === 'fulfilled') {
+      content = renderCategories();
+    } else if (categoriesStatus === 'rejected') {
+      content = categoriesError;
+    }
+
+    return (
+      <ul className="categories">
+        {content}
+      </ul>    
+    );
 }
 
 export default CategoryOptions
--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,6 +1,5 @@
 import Header from '../components/header/Header';
 import CategoryOptions from '../components/CategoryOptions';
-import Main from '../components/main/Main';
 import { Outlet } from 'react-router-dom';
 
 
