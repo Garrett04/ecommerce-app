623c4bb6b9532c9e0830965c251bd06908d41ab0
garrett04 <garrettcorreia21@gmail.com>
2024-03-13 15:58:31 +0000

    Update AddAddressForm form select dropdown functions for better readability of values sent to database

--- client/src/components/main/user/addresses/AddAddressForm.js
+++ client/src/components/main/user/addresses/AddAddressForm.js
@@ -35,30 +35,46 @@ const AddAddressForm = () => {
     const renderAllCountryOptions = () => {
         const countries = Country.getAllCountries();
         return countries.map(country => (
-            <option key={country.isoCode} value={country.isoCode}>
+            <option key={country.isoCode} value={country.name}>
                 {country.name}
             </option>
         ))
     }
 
     const renderAllStateOptions = () => {
-        const states = State.getStatesOfCountry(formData.country);
+        // if form field for country has been selected in then do this
+        if (formData.country) {
+            // finds the isoCode from the formData.country which has the name of the country 
+            // and if it is equal to the country name then return it and access the isoCode property
+            // finally store it in countryCode
+            const countryCode = Country.getAllCountries().find(country => formData.country === country.name).isoCode;
+            const states = State.getStatesOfCountry(countryCode);
 
-        return states.map(state => (
-            <option key={state.isoCode} value={state.isoCode}>
-                {state.name}
-            </option>
-        ))
+            return states.map(state => (
+                <option key={state.isoCode} value={state.name}>
+                    {state.name}
+                </option>
+            ))
+        }
     }
 
     const renderAllCityOptions = () => {
-        const cities = City.getCitiesOfState(formData.country, formData.state);
+        // if form field for country and state have been selected then do this
+        if (formData.country && formData.state) {
+            const countryCode = Country.getAllCountries().find(country => formData.country === country.name).isoCode;
+            // finds the isoCode from the formData.state which has the name of the state 
+            // and if it is equal to the state name then return it and access the isoCode property
+            // finally store it in stateCode
+            const stateCode = State.getAllStates().find(state => formData.state === state.name).isoCode;
 
-        return cities.map(city => (
-            <option key={city.name} value={city.name}>
-                {city.name}
-            </option>
-        ))
+            const cities = City.getCitiesOfState(countryCode, stateCode);
+
+            return cities.map(city => (
+                <option key={city.name} value={city.name}>
+                    {city.name}
+                </option>
+            ))
+        }
     }
 
     const handleChange = (e) => {
@@ -80,6 +96,7 @@ const AddAddressForm = () => {
                     name="address_line1"
                     value={formData.address_line1}
                     onChange={handleChange}
+                    required
                 />
                 <label htmlFor="address_line2">Address Line 2: </label>
                 <input 
@@ -88,6 +105,7 @@ const AddAddressForm = () => {
                     name="address_line2"
                     value={formData.address_line2}
                     onChange={handleChange}
+                    required
                 />
                 <label htmlFor="country">Country: </label>
                 <select  
@@ -129,6 +147,7 @@ const AddAddressForm = () => {
                     name="postal_code"
                     value={formData.postal_code}
                     onChange={handleChange}
+                    required
                 />
                 <input type="submit" value="Create new address"/>
             </form>
