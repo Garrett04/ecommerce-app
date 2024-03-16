fe70058d92b5ffce6f6e1b089cc72ebc63490456
garrett04 <garrettcorreia21@gmail.com>
2024-03-16 08:31:47 +0000

    Add default addresses above subtotal in cart details page

--- client/src/pages/Carts/CartDetails.js
+++ client/src/pages/Carts/CartDetails.js
@@ -7,11 +7,15 @@ import { setAuthToken } from "../../apis/client";
 import { createCheckoutSession } from "../../apis/checkout";
 import { getUserStatus, selectUser } from "../../features/user/userSlice";
 import { fetchUserData } from "../../apis/user";
+import { getAddressesStatus, selectAddresses } from "../../features/user/addressesSlice";
+import { fetchAddressesByUserId } from "../../apis/addresses";
 
 
 const CartDetails = () => {
     const user = useSelector(selectUser);
     const userStatus = useSelector(getUserStatus);
+    const addresses = useSelector(selectAddresses);
+    const addressesStatus = useSelector(getAddressesStatus);
 
     const [msg, setMsg] = useState("");
     const [disabled, setDisabled] = useState(false);
@@ -31,6 +35,10 @@ const CartDetails = () => {
     useEffect(() => {
       dispatch(fetchUserData());
     }, [dispatch]);
+
+    useEffect(() => {
+      dispatch(fetchAddressesByUserId());
+    }, [dispatch])
 
     const renderCart = () => {
       return cart.data.map(({
@@ -67,6 +75,32 @@ const CartDetails = () => {
       content = cartError;
     }
 
+    let default_billing_address, default_shipping_address;
+    // If addressesStatus has been fulfilled then filter for that default address
+    if (addressesStatus === 'fulfilled') {
+      default_billing_address = addresses.filter(address => address.id === user.default_billing_address_id);
+      default_shipping_address = addresses.filter(address => address.id === user.default_shipping_address_id);
+    }
+
+    // to render a default address
+    const renderDefaultAddress = (default_address) => {
+      if (addressesStatus === 'fulfilled') {
+        return default_address.map(({
+          address_line1,
+          address_line2,
+          country,
+          postal_code
+        }) => (
+          <div>
+            {address_line1}
+            {address_line2}
+            {country}
+            {postal_code}
+          </div>
+        ));
+      }
+    }
+  
     useEffect(() => {
       // If user's default billing and shipping address are not set
       // then display a message
@@ -81,6 +115,8 @@ const CartDetails = () => {
       <div className="cart">
         <h2>{cartStatus === 'fulfilled' ? cart.data[0].cart_title : null}</h2>
         {content}
+        Default Shipping Address: {renderDefaultAddress(default_shipping_address)}
+        Default Billing Address: {renderDefaultAddress(default_billing_address)}
         {cart.subtotal ? <h4>Subtotal: {cart.subtotal}</h4> : null}
         {msg}
         <button onClick={handleCheckout} disabled={disabled}>
